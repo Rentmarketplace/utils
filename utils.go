8e79ac9a7588c7449a23a7990bde8a71a6f4d2ba
Mandeep Gill <thisismyaim@gmail.com>
2021-10-26 20:20:38 +0530

    Updates expire time

--- utils.go
+++ utils.go
@@ -26,8 +26,8 @@ func init()  {
 // CreateOrUpdateToken will issue new bearer token
 func CreateOrUpdateToken(user *models.User) (map[string]string, *jwt.Token, error) {
 	var f, err = os.ReadFile(os.Getenv("CERTIFICATE_FILE"))
-	expireAt := time.Now().Add(30 * time.Second)
-	refreshExpireAt := time.Now().Add(120 * time.Second)
+	expireAt := time.Now().Add(2 * time.Minute)
+	refreshExpireAt := time.Now().Add(5 * time.Minute)
 
 	if err != nil {
 		return map[string]string{}, nil, err
